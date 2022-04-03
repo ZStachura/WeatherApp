9c453252676837d915e34a8ff2d855c3154be921
ZStachura <zmstachura@gmail.com>
2022-04-03 19:35:12 +0200

    0.1
    Just in case i mess something up

--- app/src/main/java/com/example/pogodynka/MainActivity.kt
+++ app/src/main/java/com/example/pogodynka/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.pogodynka
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity(){
+
+    val api:String="7996a2dfb0fa23e7e1b9037242e66e72"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
